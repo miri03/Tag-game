863b5f33148c09b4303df83bff1417e27deb1471
miri03 <haritmeryem@gmail.com>
2025-01-29 22:02:52 +0100

    add delete need to add search by username plus quit

--- tag-game/project/remote/static/home.js
+++ tag-game/project/remote/static/home.js
@@ -2,13 +2,13 @@
 let buttonRemote = document.getElementById('Remote')
 let buttonLocal = document.getElementById('Local')
 let buttonHistory = document.getElementById('historyButton')
+let buttonDelete = document.getElementById('delete')
 
 
 buttonRemote.addEventListener('click', (event)=>{
     let username = document.getElementById('Username').value.trim()
     if (username)
     {
-        console.log("username =>", username)
         localStorage.removeItem('username')
         localStorage.setItem('username', username)
         window.location.href = "tag.html"
@@ -22,7 +22,6 @@ buttonRemote.addEventListener('click', (event)=>{
 })
 
 buttonLocal.addEventListener('click', (event)=>{
-    console.log('click local')
     let firstP = document.getElementById('First Player').value.trim()
     let secondP = document.getElementById('Second Player').value.trim()
 
@@ -50,29 +49,45 @@ buttonLocal.addEventListener('click', (event)=>{
     }
 })
 
-buttonHistory.addEventListener('click', async(event)=>{
+buttonHistory.addEventListener('click', dispalyHistory)
+
+async function dispalyHistory()
+{
     document.getElementById('history_list').style.visibility = 'visible'
     document.getElementById('home').style.visibility = 'hidden'
     const ulElement = document.querySelector('#history_list ul')
+    while (ulElement.firstElementChild) {
+        ulElement.removeChild(ulElement.firstElementChild);
+    }
 
     let response = await fetch('http://127.0.0.1:8000/api/matchHistory/')
     const jsonData = await response.json();
     
     jsonData.forEach(game => {
         const li = document.createElement("li")
-        li.innerHTML = `
-        <span style="color: rgb(196, 16, 76);">${game.player1}</span>
-        <span style="color: rgb(220,20,60);"> L </span>
-        <span style="color: rgb(80, 200, 120);"> W </span>
-        <span style="color: rgb(32, 174, 221);">${game.player2}</span>
-        `
+        if (game.winner === game.player1)
+        {
+            li.innerHTML = `
+            <span style="color: rgb(196, 16, 76);">${game.player1}</span>
+            <span style="color: rgb(220,20,60);"> W </span>
+            <span style="color: rgb(80, 200, 120);"> L </span>
+            <span style="color: rgb(32, 174, 221);">${game.player2}</span>
+            `
+        }
+        else{
+            li.innerHTML = `
+            <span style="color: rgb(196, 16, 76);">${game.player1}</span>
+            <span style="color: rgb(220,20,60);"> L </span>
+            <span style="color: rgb(80, 200, 120);"> W </span>
+            <span style="color: rgb(32, 174, 221);">${game.player2}</span>
+            `
+        }
 
         ulElement.appendChild(li)
     });
+}
 
+buttonDelete.addEventListener("click", async()=>{
+    await fetch("http://127.0.0.1:8000/api/delete_history/", {'method':'DELETE'})
+    dispalyHistory()
 })
-
-
-// const li = document.createElement("li");
-// li.textContent = history; // Set the text content of the <li>
-// ulElement.appendChild(li); // Append the <li> to the <ul>
